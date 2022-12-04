d33a27168788a9ea4dc08dff757528d9e5275c07
stalewhitebread <111912670+stalewhitebread@users.noreply.github.com>
2022-12-04 00:06:15 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 $(function(){
   
   $('#console-box').append('console is ready...');
-  
+  /*function that reads selected info to dummy console log*/
   $('li').click(function(){
     $('#console-box').append('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
     $(this).toggleClass('selected').siblings().removeClass('selected');
