962e8a84dbf1a9f5c19ab5e48447bcd247d71a09
stalewhitebread <111912670+stalewhitebread@users.noreply.github.com>
2022-12-03 23:57:34 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 $(function(){
-  /*
+  
   $('#console-box').append('console is ready...');
-  */
+  
   $('li').click(function(){
-   // $('#console-box').append('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
+    $('#console-box').append('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
     $(this).toggleClass('selected').siblings().removeClass('selected');
 ; 
   });
